74dad0d115c9ff605e78b037689dd23bc7dd63da
Takashi Ijiri <takashi.ijiri80@gmail.com>
2020-06-24 14:44:02 +0900

    first commit
    first fommit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# SimpleWebcamSelectionDialog
-Show a dialog to select a webcom (python 3, tkinter, pillow, cv2) 
+接続されている複数のwebcameを検索・提示して，ひとつを選択するためのダイアログを表示する関数です．
+tkinter, Pillow, opencvを利用しています．
+動作確認は pycharm, python 3.8, opencv-python, Pillow環境で行なっています．
+
+使い方は
+idx = select_webcam_idx()
+とするとwebcam検索が行なわれ，ダイアログが表示されます．
+
+
---
+++ webcom_selector.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import cv2
+
+# NOTE : install cv2, Pillow, (and tkinter if necessary)
+
+
+class WebcomSelector(tk.Frame):
+    def __init__(self, _root):
+        super().__init__(_root)
+        self.root = _root
+        self.pack()
+        self.root.title("Web com selector")
+
+        # 利用できるwebcamをすべて検索 [id, img]
+        self.webcam_ids = []
+
+        for i in range(10):
+            c = cv2.VideoCapture(i, cv2.CAP_DSHOW)
+            ret, frame = c.read()
+            if ret:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                print(frame.shape, c.getBackendName())
+                self.webcam_ids.append([i, frame])
+            c.release()
+            cv2.destroyAllWindows()
+
+        # dialog 作成
+        # add radio button
+        sub_frame = tk.Frame(self.root)
+        sub_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=2)
+
+        btn_ok = tk.Button(sub_frame, text="OK", width=8, command=self.ok_button_pressed)
+        btn_ok.pack(side=tk.LEFT)
+
+        self.var_idx = tk.IntVar()
+        self.var_idx.set(0)
+        for i in range(len(self.webcam_ids)):
+            s = str(self.webcam_ids[i][0])
+            rb = tk.Radiobutton(sub_frame, text=s, value=i, variable=self.var_idx, command=self.update)
+            rb.pack(side=tk.LEFT)
+
+        # add panel
+        self.main_panel = tk.Label(self.root)
+        self.main_panel.pack()
+        self.update()
+
+    def update(self):
+        idx = self.var_idx.get()
+        print(idx)
+        imgtk = ImageTk.PhotoImage(image=Image.fromarray(self.webcam_ids[idx][1]))
+        self.main_panel.imgtk = imgtk
+        self.main_panel.configure(image=imgtk)
+
+    def ok_button_pressed(self):
+        print(self.var_idx.get())
+        self.root.destroy()
+
+    def get_selected_idx(self):
+        return self.var_idx.get()
+
+
+def select_webcam_idx():
+    dlg = WebcomSelector(_root=tk.Tk())
+    dlg.mainloop()
+    return dlg.get_selected_idx()
+
+
+if __name__ == "__main__":
+
+    idx = select_webcam_idx()
+
+    cap = cv2.VideoCapture(idx, cv2.CAP_DSHOW)
+    while 1:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    print("finish")
